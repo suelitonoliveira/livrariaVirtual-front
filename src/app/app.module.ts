0c30999d6f89030ba72cce76a80d128b13993bc7
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-04-21 18:53:32 -0300

    home implementado com rota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,16 @@ import { NavComponent } from './components/template/nav/nav.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './components/views/home/home.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
     
   ],
   imports: [
@@ -27,7 +30,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
