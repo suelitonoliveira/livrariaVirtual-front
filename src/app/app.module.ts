fa92ddae2e1723c730a6164cb62cbaab278e6714
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-04-22 22:42:09 -0300

    adicionando end point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +36,8 @@ import {MatTableModule} from '@angular/material/table';
     MatListModule,
     MatIconModule,
     MatCardModule,
-    MatTableModule
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../categoria.model';
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categoria-read',
@@ -7,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaReadComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  categorias: Categoria[] = []
 
   displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+
+  constructor(private service: CategoriaService) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(){
+    this.service.findAll().subscribe(resposta => {
+      console.log(resposta);
+      this.categorias = resposta;
+    })
+  }
 
 }
