3c6e5d3ba47ef754a511638a12522d984c6411cd
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-05-10 14:58:48 -0300

    criado componente para listar livros

---
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-read-all',
+  templateUrl: './livro-read-all.component.html',
+  styleUrls: ['./livro-read-all.component.css']
+})
+export class LivroReadAllComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'titulo', 'livros', 'acoes'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
