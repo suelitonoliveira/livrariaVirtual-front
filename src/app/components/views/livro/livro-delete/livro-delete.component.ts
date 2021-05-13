6bbbb1e3a92d7574a664751dbb61397275128879
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-05-13 16:36:09 -0300

    criando componente de delete livro

---
+++ src/app/components/views/livro/livro-delete/livro-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-delete',
+  templateUrl: './livro-delete.component.html',
+  styleUrls: ['./livro-delete.component.css']
+})
+export class LivroDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
